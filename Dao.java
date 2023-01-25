9d1f89e3d6f66bdf4f4a706e4af155ecb1e14430
yzy <yzy106@outlook.com>
2023-01-25 18:15:49 +0800

    新增了dao和mainsvc类

---
+++ Dao.java
@@ -0,0 +1,5 @@
+public class Dao {
+    public static void main(String[] args) {
+        System.out.println("hello dao");
+    }
+}
---
+++ MainSvc.java
@@ -0,0 +1,5 @@
+public class MainSvc {
+    public static void main(String[] args) {
+        System.out.println("hello mainsvc");
+    }
+}
