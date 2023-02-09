236ba0f1b1ecbf1f9a3efb5d423db9b069ec5a43
yzy <yzy106@outlook.com>
2023-02-09 13:35:16 +0800

    test

--- HelloTest.java
+++ HelloTest.java
@@ -1,5 +1,8 @@
+import test.java.PkgHello;
+
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world by yzy");
+        System.out.println(PkgHello.pkgName());
     }
 }
