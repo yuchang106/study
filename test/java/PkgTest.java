f72d9f74e39a5464b6d2386232c85cdc427c5416
yzy <yzy106@outlook.com>
2023-02-09 14:30:52 +0800

    test

--- test/java/PkgTest.java
+++ test/java/PkgTest.java
@@ -3,12 +3,13 @@ package test.java;
 public class PkgTest {
     public static void main(String[] args) {
         System.out.println("hello PkgTest");
-        System.out.println(PkgHello.pkgName());
+        PkgHello pkgHello = new PkgHello();
+        System.out.println(pkgHello.pkgName());
     }
 }
 
 class PkgHello {
-    public static String pkgName() {
+    public String pkgName() {
         return "this is a PkgHello";
     }
 }
