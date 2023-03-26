b16f17b67a04ff6e86b4ea7dd0d4a106d85693ba
yzy <yzy106@outlook.com>
2023-03-26 13:15:20 +0800

    修改模版方法

--- lesson/Student.java
+++ lesson/Student.java
@@ -2,7 +2,7 @@ package lesson;
 
 public abstract class Student {
     public final void write() {
-        System.out.println("this is the head of the title");
+        System.out.println("this is the title of the article");
         writeBody("hello, this is the body");
         System.out.println("this is the bottom");
         System.out.println("the end");
