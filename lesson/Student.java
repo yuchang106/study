a9dd0f78fc39ce12a3114165c2577798ca0839cf
yzy <yzy106@outlook.com>
2023-12-09 23:02:14 +0800

    修改抽象类Student

--- lesson/Student.java
+++ lesson/Student.java
@@ -5,6 +5,7 @@ public abstract class Student {
         System.out.println("this is the title of the article");
         writeBody("hello, this is the body");
         System.out.println("this is the bottom");
+        System.out.println("the end");
     }
 
     public abstract void writeBody(String str);
